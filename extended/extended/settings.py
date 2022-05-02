5e7d661a3612faffaafa2eae2384c7bbbbd816f0
Brian Baker <brian@holisticdeveloper.com>
2022-05-02 00:30:10 -0400

    turn off django debug

--- extended/extended/settings.py
+++ extended/extended/settings.py
@@ -5,7 +5,7 @@ dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, os.pa
 sys.path.append(dir)
 import sitesettings
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -27,7 +27,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["citadel", "stats.drfriendless.com", "stats6.drfriendless.com", "localhost"]
+ALLOWED_HOSTS = ["citadel", "extstats.bakerhalfdozen.com", "localhost"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
